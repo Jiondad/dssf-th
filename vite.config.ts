098f314f6935c3321cc8ad19ab0be60ba235fb25
Jiondad <shinekjh@gmail.com>
2026-07-14 14:52:20 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: process.env.NODE_ENV === 'production' ? '/dssf-th/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
